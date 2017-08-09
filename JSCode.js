bbc10e1a938a2846e27d730d810108aecdbc442c
brhoton <30869936+brhoton@users.noreply.github.com>
2017-08-09 13:24:09 -0500

    Added and Initialized JavaScript source file

---
+++ JSCode.js
@@ -0,0 +1,15 @@
+    function playGame()
+    {
+      let x = 0;
+      let y = 0;
+      x = (prompt('Enter a number.'));
+      x = parseInt(x);
+      
+      if (isNaN(x)) {
+        document.getElementById('output').innerHTML = x + ' is not a number! Please enter a positive integer.'; }
+      else if (x < 0) {
+        document.getElementById('output').innerHTML = x + ' is a negative number! Please enter a positive number.'; }
+      else if {
+        y = x * 5;
+        documnet.getElementById('output').innerHTML = 'Result ' + y; }
+    }
